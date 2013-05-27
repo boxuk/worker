e05db663fb86c203a1b4cd6bdb556bf798563766
rod <rod@pu-gh.com>
2013-05-27 22:37:08 +0100

    bump clojure to 1.5.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 
 (defproject boxuk/worker "0.3.0"
   :description "Simple parallel job manager"
-  :dependencies [[org.clojure/clojure "1.5.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
 
